49f6421545c08d4b6421416b99d9f95a2244cd5c
Anas Khan <builderanas420@gmail.com>
2024-07-13 07:01:48 -0700

    change

--- p.js
+++ p.js
@@ -2,6 +2,7 @@
 var input = document.getElementById('input')
 
 function showinput(value) {
+
     input.value += value
 }
 
@@ -9,3 +10,5 @@ function calc() {
     var calculate = eval(input.value) 
     input.value = calculate
 }
+var del = document.getElementById("del")
+del.value = ""
